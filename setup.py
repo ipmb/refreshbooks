746816ea80e9f3173c783701b62460b527955949
Owen Jacobson <owen@freshbooks.com>
2009-12-02 12:59:57 -0500

    Setuptools bootstrap glue, since this is a public project and users might concievably not have setuptools installed. Lame, but necessary.

--- setup.py
+++ setup.py
@@ -1,4 +1,9 @@
-from setuptools import setup, find_packages
+try:
+    from setuptools import setup, find_packages
+except ImportError:
+    from ez_setup import use_setuptools
+    use_setuptools()
+    from setuptools import setup, find_packages
 
 setup(
     name='refreshbooks',
@@ -80,8 +85,13 @@ setup(
         'Topic :: Software Development :: Libraries :: Python Modules'
     ],
     
-    packages = find_packages(exclude=['*.test', '*.test.*']),
+    packages=[
+        'refreshbooks'
+    ],
     
+    setup_requires=[
+        'setuptools-git'
+    ],
     tests_require=[
         'nose',
         'mock'
