9d2d2022d2d6b4e72cdc010b2e3014563ec720de
Owen Jacobson <owen@freshbooks.com>
2009-12-04 15:55:57 -0500

    Provide options and docs for logging request/response XML.

--- refreshbooks/api.py
+++ refreshbooks/api.py
@@ -1,3 +1,4 @@
+import sys
 import functools
 
 import functional
@@ -14,7 +15,27 @@ def api_url(domain):
     """
     return "https://%s/api/2.1/xml-in" % (domain, )
 
-def AuthorizingClient(domain, auth):
+default_request_encoder = adapters.xml_request
+default_response_decoder = functional.compose(
+    adapters.fail_to_exception_response,
+    objectify.fromstring
+)
+
+def logging_request_encoder(method, **params):
+    encoded = default_request_encoder(method, **params)
+    
+    print sys.stderr, "--- Request (%r, %r) ---" % (method, params)
+    print encoded
+    
+    return encoded
+
+def logging_response_decoder(response):
+    print sys.stderr, "--- Response ---"
+    print response
+    
+    return default_response_decoder(response)
+
+def AuthorizingClient(domain, auth, request_encoder, response_decoder):
     """Creates a Freshbooks client for a freshbooks domain, using
     an auth object.
     """
@@ -22,33 +43,63 @@ def AuthorizingClient(domain, auth):
     http_transport = transport.HttpTransport(api_url(domain), auth)
     
     return client.Client(
-        adapters.xml_request,
+        request_encoder,
         http_transport,
-        functional.compose(
-            adapters.fail_to_exception_response,
-            objectify.fromstring
-        )
+        response_decoder
     )
 
-def TokenClient(domain, token):
+def TokenClient(
+    domain,
+    token,
+    request_encoder=default_request_encoder,
+    response_decoder=default_response_decoder
+):
     """Creates a Freshbooks client for a freshbooks domain, using
     token-based auth.
+    
+    The optional request_encoder and response_decoder parameters can be
+    passed the logging_request_encoder and logging_response_decoder objects
+    from this module, or custom encoders, to aid debugging or change the
+    behaviour of refreshbooks' request-to-XML-to-response mapping.
     """
     
-    return AuthorizingClient(domain, transport.TokenAuthorization(token))
+    return AuthorizingClient(
+        domain,
+        transport.TokenAuthorization(token),
+        request_encoder,
+        response_decoder
+    )
 
-def OAuthClient(domain, consumer_key, consumer_secret, token, token_secret):
+def OAuthClient(
+    domain,
+    consumer_key,
+    consumer_secret,
+    token,
+    token_secret,
+    request_encoder=default_request_encoder,
+    response_decoder=default_response_decoder
+):
     """Creates a Freshbooks client for a freshbooks domain, using
     OAuth. Token management is assumed to have been handled out of band.
+    
+    The optional request_encoder and response_decoder parameters can be
+    passed the logging_request_encoder and logging_response_decoder objects
+    from this module, or custom encoders, to aid debugging or change the
+    behaviour of refreshbooks' request-to-XML-to-response mapping.
     """
     
     consumer = oauth.OAuthConsumer(consumer_key, consumer_secret)
     token = oauth.OAuthToken(token, token_secret)
     
-    return AuthorizingClient(domain, transport.OAuthAuthorization(
-        consumer,
-        token
-    ))
+    return AuthorizingClient(
+        domain,
+        transport.OAuthAuthorization(
+            consumer,
+            token
+        ),
+        request_encoder,
+        response_decoder
+    )
 
 def list_element_type(_name, **kwargs):
     """Convenience function for creating tuples that satisfy
