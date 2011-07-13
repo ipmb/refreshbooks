2f6fc05e89b00379ab54882b4e3ea886cc22daec
David <dev@longword.ca>
2011-07-13 14:20:06 -0700

    fixed typos, edits.

--- setup.py
+++ setup.py
@@ -85,13 +85,13 @@ setup(
     If you are having trouble accessing items as in:
 
         items_response = c.items.list()
-        for item in item_response.items.item:
+        for item in items_response.items.item:
             print item.item_id
     
-    Adjust your syntax to the following:
+    Adjust your syntax to use dictionary item lookup:
 
         items_response = c.items.list()
-        for item in item_response['items'].item:
+        for item in items_response['items'].item:
             print item.item_id
 
     ObjectifiedDataElement provides a method named items which shadows the 
